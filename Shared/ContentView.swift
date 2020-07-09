2b78d24a6178504d779c621a5ba3b5bebb5d3e49
Somendra Yadav <somendra.sy@gmail.com>
2020-07-10 02:04:24 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Apple on 10/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2App.swift
+//  Shared
+//
+//  Created by Apple on 10/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
